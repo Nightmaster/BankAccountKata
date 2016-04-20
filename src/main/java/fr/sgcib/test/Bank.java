4c647b97917892966996ccaf14e6329b13ea6066
Nightmaster SEI'TAER <nightmaster@seitaer.com>
2016-04-20 22:21:21 +0200

    Create first Bank test and corresponding code in main class

--- src/main/java/fr/sgcib/test/Bank.java
+++ src/main/java/fr/sgcib/test/Bank.java
@@ -1,4 +1,16 @@
 package fr.sgcib.test;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Bank {
+	private final Map<Long, Client> clients;
+
+	public Bank() {
+		this.clients = new TreeMap<Long, Client>();
+	}
+
+	public Map<Long, Client> getClients() {
+		return this.clients;
+	}
 }
--- src/test/java/fr/sgcib/test/BankTest.java
+++ src/test/java/fr/sgcib/test/BankTest.java
@@ -1,4 +1,14 @@
 package fr.sgcib.test;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotEquals;
+
 public class BankTest {
+	@Test
+	public void getClientListTest() {
+		final Bank bank = new Bank();
+
+		assertNotEquals(0, bank.getClients().size());
+	}
 }
