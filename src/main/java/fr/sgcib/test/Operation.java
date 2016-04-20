8fb4601fdfa691b6f10c8daeea88e115a2c8537d
Nightmaster SEI'TAER <nightmaster@seitaer.com>
2016-04-20 23:07:49 +0200

    Write test on Operation initialization and constructor and getter for operation

--- src/main/java/fr/sgcib/test/Operation.java
+++ src/main/java/fr/sgcib/test/Operation.java
@@ -1,5 +1,42 @@
 package fr.sgcib.test;
 
-public class Operation {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+public class Operation {
+	private final String label;
+	private final LocalDate date;
+	private final LocalTime hour;
+	private final BigDecimal previousAmount, newAmount;
+
+	public Operation(final String label, final BigDecimal previousAmount, final BigDecimal newAmount) {
+		this.label = label;
+		this.previousAmount = previousAmount;
+		this.newAmount = newAmount;
+		this.date = LocalDate.now();
+		this.hour = LocalTime.now();
+	}
+
+	public LocalDate getDate() {
+		return this.date;
+	}
+
+	public LocalTime getHour() {
+		return this.hour;
+	}
+
+	public String getLabel() {
+		return this.label;
+	}
+
+	public BigDecimal getNewAmount() {
+		return this.newAmount;
+	}
+
+	public BigDecimal getPreviousAmount() {
+		return this.previousAmount;
+	}
 }
--- src/test/java/fr/sgcib/test/OperationTest.java
+++ src/test/java/fr/sgcib/test/OperationTest.java
@@ -1,5 +1,65 @@
 package fr.sgcib.test;
 
-public class OperationTest {
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class OperationTest {
+	@Test
+	public void throwIAEOnIncorrectInitialization() {
+		final List<Exception> exceptions = new ArrayList<>();
+		final String emptyLabel = "",
+			label = "label";
+		final BigDecimal zero = BigDecimal.ZERO,
+			one = BigDecimal.ONE;
+
+
+		// Try incorrect label
+		try {
+			new Operation(emptyLabel, zero, one);
+		}
+		catch (final Exception iae) {
+			assertNotNull(iae);
+			exceptions.add(iae);
+		}
+
+		// Try incorrect previousAmount
+		try {
+			new Operation(label, null, one);
+		}
+		catch (final Exception iae) {
+			assertNotNull(iae);
+			exceptions.add(iae);
+		}
+
+		// Try incorrect newAmount
+		try {
+			new Operation(label, zero, null);
+		}
+		catch (final Exception iae) {
+			assertNotNull(iae);
+			exceptions.add(iae);
+		}
+
+		// Try with all incorrect parameters
+		try {
+			new Operation(emptyLabel, null, null);
+		}
+		catch (final Exception iae) {
+			assertNotNull(iae);
+			exceptions.add(iae);
+		}
+
+		checkExceptions(exceptions);
+	}
+
+	private static void checkExceptions(final List<Exception> exceptions) {
+		assertTrue("There only " + exceptions.size() + " that has been thrown, on 4 expected!", 4 == exceptions.size());
+		exceptions.forEach(exc -> assertNotNull("Exception " + exceptions.indexOf(exc) + " is null!", exc));
+		exceptions.forEach(exc -> assertTrue("Exception " + exceptions.indexOf(exc) + " is not an IllegalArgumentException!", exc instanceof  IllegalArgumentException));
+	}
 }
