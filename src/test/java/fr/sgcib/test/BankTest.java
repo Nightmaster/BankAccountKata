5cca907f8b1e1f7ceb75e68ea2d6899cdbfe3e4d
Nightmaster SEI'TAER <nightmaster@seitaer.com>
2016-04-21 13:02:53 +0200

    Add call of `createClient()` in Bank test

--- src/test/java/fr/sgcib/test/BankTest.java
+++ src/test/java/fr/sgcib/test/BankTest.java
@@ -1,5 +1,6 @@
 package fr.sgcib.test;
 
+import java.math.BigDecimal;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotEquals;
@@ -8,6 +9,8 @@ public class BankTest {
 	@Test
 	public void getClientListTest() {
 		final Bank bank = Bank.getInstance();
+
+		bank.createClient(true, "TEST", new String[] {"Tester"}, "tester@test.fr", "1 road of the Imagination, 4242 SF City", new BigDecimal(1000), 500L);
 
 		assertNotEquals(0, bank.getClients().size());
 	}
