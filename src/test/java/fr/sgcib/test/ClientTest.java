20c4975e4b79bdf1ae294fbb8892918028bc5ee3
Nightmaster SEI'TAER <nightmaster@seitaer.com>
2016-04-21 15:39:19 +0200

    Add the `trimOnInitAndSet()` function in Client test

--- src/test/java/fr/sgcib/test/ClientTest.java
+++ src/test/java/fr/sgcib/test/ClientTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Set;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static fr.sgcib.test.constants.LogHelper.LF;
@@ -15,7 +16,9 @@ import static fr.sgcib.test.utils.TestUtils.addAndCheck;
 import static fr.sgcib.test.utils.TestUtils.createAccount;
 
 public class ClientTest {
-	private static final String FAMILY_NAME = "Test",
+	private static final String TAB = "\t",
+		SPACE = " ",
+		FAMILY_NAME = "Test",
 		EMAIL_ADDRESS = "alice@test.fr",
 		PHYSICAL_ADDRESS = "Cul-de-sac, The Shire";
 	private static final String[] NAMES = new String[] {"Alice", "Wonderland"};
@@ -72,5 +75,35 @@ public class ClientTest {
 		catch (final Throwable th) {
 			addAndCheck(throwables, th);
 		}
+	}
+
+	@Test
+	public void trimOnInitAndSet() {
+		final String testCase = "trimOnInitAndSet() - ";
+		final Client client;
+		final Set<Client> clients = new HashSet<>(4);
+		final String[] names = {SPACE + NAMES[0] + TAB, TAB + SPACE + NAMES[1] + SPACE + TAB};
+		long it = 0L;
+
+		System.out.println(LF + SEPARATOR + testCase + "Check trim is well done in constructor");
+		clients.add(new Client(--it, false, SPACE + FAMILY_NAME + TAB, NAMES, EMAIL_ADDRESS, PHYSICAL_ADDRESS, ACCOUNTS));
+		clients.add(new Client(--it, false, FAMILY_NAME, names, EMAIL_ADDRESS, PHYSICAL_ADDRESS, ACCOUNTS));
+		clients.add(new Client(--it, false, FAMILY_NAME, NAMES, TAB + EMAIL_ADDRESS + SPACE, PHYSICAL_ADDRESS, ACCOUNTS));
+		clients.add(new Client(--it, false, FAMILY_NAME, NAMES, EMAIL_ADDRESS, SPACE + PHYSICAL_ADDRESS, ACCOUNTS));
+
+		clients.forEach(current -> {
+			assertEquals("Family name is not the same!", FAMILY_NAME, current.getLastName());
+			for (int i = 0; i < NAMES.length; i++)
+				assertEquals("Name n" + i + " are not the same!", NAMES[i], current.getNames().get(i));
+			assertEquals("Email address is not the same", EMAIL_ADDRESS, current.getEmailAddress());
+			assertEquals("Physical address is not the same", PHYSICAL_ADDRESS, current.getPhysicalAddress());
+		});
+		System.out.println(testCase + "Check on constructor is OK!\nNow let's check on setters");
+		client = new Client(--it, false, FAMILY_NAME, NAMES, EMAIL_ADDRESS, PHYSICAL_ADDRESS, ACCOUNTS);
+		client.setEmailAddress(SPACE + EMAIL_ADDRESS + TAB);
+		assertEquals("Email address is not the same after set", EMAIL_ADDRESS, client.getEmailAddress());
+		client.setPhysicalAddress(SPACE + PHYSICAL_ADDRESS + TAB);
+		assertEquals("Email address is not the same after set", PHYSICAL_ADDRESS, client.getPhysicalAddress());
+		System.out.println(testCase + OK + LF + SEPARATOR);
 	}
 }
