f937cbca5f8d5cf02005ee8287bf443a6d4447df
Nightmaster SEI'TAER <nightmaster@seitaer.com>
2016-04-21 12:53:54 +0200

    Make the Bank class a thread safe Singleton

--- src/main/java/fr/sgcib/test/Bank.java
+++ src/main/java/fr/sgcib/test/Bank.java
@@ -4,10 +4,17 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Bank {
+	private static final Bank INSTANCE = new Bank();
+	private static long clientCount = 0,
+		accountCount = 0;
 	private final Map<Long, Client> clients;
 
-	public Bank() {
+	private Bank() {
 		this.clients = new TreeMap<Long, Client>();
+	}
+
+	public static Bank getInstance(){
+		return INSTANCE;
 	}
 
 	public Map<Long, Client> getClients() {
